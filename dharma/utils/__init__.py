25afae300745cf1adae8cd597c68c071e1436110
Lukasz Haze <lhaze@lhaze.name>
2016-02-11 00:38:56 +0100

    Reorganization of utils
    * Turning utils to package
    * OrderedSet introduced

---
+++ dharma/utils/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from .collections import frozendict, OrderedSet
+from .inspect import is_argspec_valid
+
+__all__ = ['frozendict', 'is_argspec_valid', 'OrderedSet']
---
+++ dharma/utils/inspect.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+def is_argspec_valid(function, arg_number=None, kwargs_names=None):
+    """
+    Args:
+        function - function to be checked.
+        arg_number - number of all arguments (named or not) which is going to
+            be used calling the function.
+        kwargs_names - iterable of names for keyword arguments whose existence
+            in signature is going to be verified.
+
+    Returns:
+        bool - True iff function is valid to be valid with specified arg
+            number and keyword arguments names.
+    """
+    # TODO if six.PY3 use SignatureObject
+    # else:
+    # import inspect
+    # (len(inspect.getargspec(listener).args) == 3 or
+    #  inspect.getargspec(listener).varargs is not None)
+    return True
