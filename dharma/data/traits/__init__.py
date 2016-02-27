8e9c155c59bc0a6409f3a2600a4bea8190c3a4c4
Lukasz Haze <lhaze@lhaze.name>
2016-02-27 16:28:44 +0100

    InMemoryRepository p. 1

--- dharma/data/traits/__init__.py
+++ dharma/data/traits/__init__.py
@@ -1,8 +1,13 @@
 from .base import Trait  # noqa
 from .nature import Nature  # noqa
 from .simple_traits import Int, Long, Float, Complex, Text
+# from .collection import Mapping, Sequence
+# from .type_based import Instance, Type
 
+
+Mapping = Sequence = Instance = Type = lambda x: x
 
 SIMPLE_TRAITS = [Int, Text, Long, Float, Complex]
 ENUMERATE_TRAITS = []
-COLLECTION_TRAITS = []
+COLLECTION_TRAITS = [Mapping, Sequence]
+TYPE_BASED_TRAITS = [Instance, Type]
