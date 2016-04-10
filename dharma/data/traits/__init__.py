bee46e5f97d6dd0bbfcdbcd675b12077a7077497
Lukasz Haze <lhaze@lhaze.name>
2016-04-11 01:16:57 +0200

    InMemoryRepository p. 2
    * fixes
    * tests
    * removed abstract InMemoryRepository.load as its not the in memory consideration

--- dharma/data/traits/__init__.py
+++ dharma/data/traits/__init__.py
@@ -1,10 +1,7 @@
+# -*- coding: utf-8 -*-
 from .base import undefined_value, Trait  # noqa
 from .nature import Nature  # noqa
-from .simple_traits import Int, Long, Float, Complex, Text
+from .simple_traits import Int, Long, Float, Complex, Text  # noqa
 
-Mapping = Sequence = Instance = Type = lambda x: x
 
-SIMPLE_TRAITS = [Int, Text, Long, Float, Complex]
-ENUMERATE_TRAITS = []
-COLLECTION_TRAITS = [Mapping, Sequence]
-TYPE_BASED_TRAITS = [Instance, Type]
+Mapping = Sequence = Instance = Type = lambda *args, **kwargs: None
