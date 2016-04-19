e201d396e45c9fce22b2958a907108f6188ad2f3
Lukasz Haze <lhaze@lhaze.name>
2016-04-19 02:59:53 +0200

    dharma.data.Trait: validation p. 2

--- tests/data/traits/validation.py
+++ tests/data/traits/validation.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from mock import Mock
-import pytest
+# import pytest
 
 from dharma.utils.tests.factories import nature_class_factory
 
 
-@pytest.fixture
-def nature_instance_with_validator():
+def test_validation_is_fired():
+    "Tests if validators passed with `validators` arguemnt are fired"
     mock = Mock()
 
     def validator(instance, new_value):
@@ -16,12 +16,36 @@ def nature_instance_with_validator():
         trait_name='a_trait', nature_name='ANature',
         kwargs={'validators': [validator]}
     )
-    nature_class.mock = mock
-    return nature_class()
+    instance = nature_class()
 
-
-def test_validation_is_fired(nature_instance_with_validator):
-    instance = nature_instance_with_validator
     value = 'value to validate'
     instance.a_trait = value
-    instance.mock.assert_called_once_with(instance, value)
+    mock.assert_called_once_with(instance, value)
+
+
+def test_construct_validators(mocker):
+    "Tests if genus is used to build validators"
+    genus = object()
+    with mocker.patch('dharma.data.traits.base.construct_validators'):
+        nature_class = nature_class_factory(
+            trait_name='a_trait', nature_name='ANature',
+            kwargs={'genus': genus}
+        )
+        from dharma.data.traits.base import construct_validators
+        construct_validators.assert_called_once_with(genus)
+
+
+def test_validators_from_genus(mocker):
+    "Tests if validators, built with construct_validators, are fired"
+    genus = object()
+    validator_mock = mocker.MagicMock()
+    with mocker.patch('dharma.data.traits.base.construct_validators',
+                      return_value=[validator_mock]):
+        nature_class = nature_class_factory(
+            trait_name='a_trait', nature_name='ANature',
+            kwargs={'genus': genus}
+        )
+        instance = nature_class()
+        value = 'value to validate'
+        instance.a_trait = value
+        validator_mock.assert_called_once_with(instance, value)
