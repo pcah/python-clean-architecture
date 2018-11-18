35fd1b88599b88e55d85375b07f087b3b51836d4
lhaze <lhaze@lhaze.name>
2018-11-18 02:28:10 +0100

    Refs #6. Preparing the setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import sys
 
-from setuptools import setup, find_packages
+from setuptools import (
+    setup,
+    find_packages,
+)
 from setuptools.command.test import test as TestCommand
 
+
 NAME = 'pca'
-VERSION = (0, 0, 1, 'dev')
+VERSION = (0, 0, 1)
 INSTALL_REQUIRES = (
     'traitlets>=4.3.2',
     'six',
@@ -28,12 +32,37 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 if __name__ == '__main__':
     setup(
         name=NAME,
         version='.'.join(str(i) for i in VERSION),
+        url=f'https://github.com/pcah/{NAME}',
+        license='MIT License',
+        author='lhaze',
+        author_email='lhaze@lhaze.name',
+        description='A framework-agnostic toolkit for Python driven by the Clean Architecture ',
+        long_description_content_type='text/markdown',
+        long_description=readme(),
+        platforms='any',
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Operating System :: OS Independent",
+            "Topic :: Software Development",
+            "Intended Audience :: Developers",
+            "Development Status :: 2 - Pre-Alpha"
+        ],
+
         cmdclass={'test': PyTest},
         install_requires=INSTALL_REQUIRES,
         tests_require=TEST_REQUIRE,
-        packages=find_packages(exclude=['tests']),
+        packages=find_packages(),
     )
