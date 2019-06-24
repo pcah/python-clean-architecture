9f503c6e54686f0fe48a3f68379bfda98dc5bb23
lhaze <lhaze@lhaze.name>
2019-06-24 04:11:01 +0200

    Bump version: 0.0.3 -> 0.0.4

--- pca/__init__.py
+++ pca/__init__.py
@@ -5,7 +5,7 @@ from devops.utils.version import Version
 
 PROJECT_NAME = 'python-clean-architecture'
 PACKAGE_NAME = 'pca'
-VERSION = Version(0, 0, 3)
+VERSION = Version(0, 0, 4)
 
 PACKAGE_DIR = path.dirname(__file__)
 PROJECT_DIR = path.dirname(PACKAGE_DIR)
